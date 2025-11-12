12e6f3311676a5d41d025a24c67b83f9d3467125
BenFlpoly <brosenstock9860@floridapoly.edu>
2025-11-12 20:14:56 +0000

    randomness created

--- assignment/game.py
+++ assignment/game.py
@@ -1,1 +1,10 @@
+import random
+
 print("Welcome to Rock-Paper-Scissors")
+
+    choices="rock","paper","scissors"]]
+
+Computer_choice=random.choice(choices)
+print(f"the computer chose {Computer_choice}")
+
+
